628e78f27d303ec95fbe0c6dbb12195104e86e65
Jon Phoenix <jonathan.martb@gmail.com>
2020-10-19 14:26:40 -0700

    Adds models index and workout, creates workout schema

---
+++ models/workout.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose'); // Importing mongoose
+
+const Schema = mongoose.Schema; // Schema declaration
+
+const workoutSchema = new Schema ({ // Creating a new schema object
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            type: { type: String, trim: true },
+            name: { type: String, trim: true },
+            duration: { type: Number },
+            weight: { type: Number },
+            reps: { type: Number },
+            sets: { type: Number },
+            distance: { type: Number }
+        }
+    ]
+},
+{
+    toJSON: { // Adds a virtual property not stored in Mongo
+        virtuals: true
+    }
+});
+
+// Adds a virtual property to schema, this reduces the array of
+// exercises to the sum of their total duration.
+workoutSchema.virtual('totalDuration').get(function () {
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0);
+});
+
+const workout = mongoose.model('Workout', workoutSchema);
+
+module.exports = Workout;
