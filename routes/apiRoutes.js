8670c0645817dbbb43316acefac31929e47cada6
Jon Phoenix <jonathan.martb@gmail.com>
2020-10-23 18:12:20 -0700

    Adds alternative approaches to app.put route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -31,6 +31,43 @@ module.exports = (app) => {
         });
     });
 
+
+    // app.put('/api/workouts/:id', ({ body, params }, res) => {
+
+    //     if (body.name) {
+    //         db.Workout.findByIdAndUpdate
+    //         (
+    //             { _id: params.id },
+    //             { $push: {exercises: body} },
+
+    //             function (err, result) {
+    //                 if (err) {
+    //                     res.json(err)
+    //                 } else {
+    //                     res.json(result)
+    //                 };
+    //             }
+    //         )
+    //     }
+    // });
+
+    // app.put('/api/workouts/:id', ({ body, params }, res) => {
+
+    //     if (body.name) {
+    //         db.Workout.findByIdAndUpdate
+    //         (
+    //             { _id: params.id },
+    //             { $push: {exercises: body} },
+    //         )
+    //         .then(dbWorkout => {
+    //             res.json(dbWorkout);
+    //         })
+    //         .catch(err => {
+    //             res.status(400).json(err);
+    //         });
+    //     }
+    // });
+
     app.put('/api/workouts/:id', (req, res) => {
         db.Workout.findByIdAndUpdate
         (
